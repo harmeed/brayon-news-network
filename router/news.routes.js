a84bcfb91f43d8760b0ef7be6ff2daff4e795b20
hammed <hammedolalekan60@gmail.com>
2022-08-30 10:39:05 +0100

    brayon news

--- router/news.routes.js
+++ router/news.routes.js
@@ -5,7 +5,7 @@ const { authenticate } = require("../middleware/auth");
 const { postNews, getNews, getNewsByTitle } = require("../controllers/news.controller");
 const { updateExistingNews } = require("../controllers/news.controller");
 const {deleteNews} = require("../controllers/news.controller");
-// const { updateNews } = require("../controllers/user.controller");
+
 
 router.post("/create", authenticate, postNews);
 router.put("/update/:id", authenticate, updateExistingNews);
